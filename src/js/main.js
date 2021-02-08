5a413236344f3f8cd9d3cdb0b0bab61e0f67416e
ctalloc <cecilia.tallo@gmail.com>
2021-02-08 18:25:25 +0100

    Implemented search and showing results

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,58 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+//js-results js-form js-searchInput js-searchBtn
+
+//API
+
+//SEARCH
+
+const formElement = document.querySelector(".js-form");
+const btnElement = document.querySelector(".js-searchBtn");
+const inputElement = document.querySelector(".js-searchInput");
+const resultsElement = document.querySelector(".js-results");
+
+//API
+
+function callToApi(searchValue) {
+  fetch("http://api.tvmaze.com/search/shows?q=" + searchValue)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      renderResult(data);
+    });
+}
+
+//PAINT
+
+function renderResult(series) {
+  let htmlCode = '';
+  for (const serie of series) {
+    const showItem = serie.show;
+    const showTitle = showItem.name;
+    const showImages = showItem.image;
+    const showThumbnail = `${showImages.medium}`;
+    console.log(showThumbnail);
+    htmlCode += '<li>';
+    htmlCode += `<img src="${showThumbnail}">`;
+    htmlCode += `<p>${showTitle}</p>`;
+    htmlCode += '</li>';
+  }
+  resultsElement.innerHTML = htmlCode;
+}
+
+//PREVENT DEFAULT FORM BTN
+
+function handleForm(ev) {
+  ev.preventDefault();
+  console.log("No hago submit.");
+}
+
+formElement.addEventListener("submit", handleForm);
+
+//HANDLE SUBMIT BUTTON CLICK
+
+function handleSubmitButton() {
+  callToApi(inputElement.value);
+}
+
+btnElement.addEventListener("click", handleSubmitButton);
