338fac0994022b5312f64493f8af75993eba9890
ctalloc <cecilia.tallo@gmail.com>
2021-02-09 10:38:44 +0100

    Implemented function to add to favorite list when clicked on.

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,9 @@ const formElement = document.querySelector(".js-form");
 const btnElement = document.querySelector(".js-searchBtn");
 const inputElement = document.querySelector(".js-searchInput");
 const resultsElement = document.querySelector(".js-results");
+const favoritesListElement = document.querySelector('.js-favoritesList');
+let shows = [];
+let favoriteShows = [];
 
 //API
 
@@ -17,7 +20,8 @@ function callToApi(searchValue) {
   fetch("http://api.tvmaze.com/search/shows?q=" + searchValue)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      shows = data;
+      //console.log(shows);
       renderResult(data);
     });
 }
@@ -29,6 +33,7 @@ function renderResult(series) {
   for (const serie of series) {
     const showItem = serie.show;
     const showTitle = showItem.name;
+    const showId = showItem.id;
     let showImages = showItem.image;
     const showThumbnail = isImgUrlValid();
     function isImgUrlValid() {
@@ -41,12 +46,13 @@ function renderResult(series) {
       }
       return validUrl;
     }
-    htmlCode += '<li class="resultList--item">';
+    htmlCode += `<li class="resultList--item js-show" id="${showId}">`;
     htmlCode += `<img src="${showThumbnail}" alt="${showTitle} poster">`;
     htmlCode += `<p>${showTitle}</p>`;
     htmlCode += "</li>";
   }
   resultsElement.innerHTML = htmlCode;
+  listenShowsEvents()
 }
 
 //PREVENT DEFAULT FORM BTN
@@ -65,3 +71,53 @@ function handleSubmitButton() {
 }
 
 btnElement.addEventListener("click", handleSubmitButton);
+
+//SHOW LIST LISTENER
+
+function listenShowsEvents() {
+  const showElements = document.querySelectorAll('.js-show');
+  for (const showElement of showElements) {
+    showElement.addEventListener('click', handleShow);
+  }
+}
+
+function handleShow(ev) {
+  const clickedShow = ev.currentTarget;
+  const clickedShowId = clickedShow.id;
+  console.log(clickedShowId);
+  const resultShows = shows; 
+  console.log(resultShows);
+  for(const result of resultShows){
+    const showItem = result.show;
+    const itemId = showItem.id;
+    const stringId = `${itemId}`;
+    if (stringId === clickedShowId){
+      console.log("te encontré");
+      favoriteShows.push(result)
+    }
+  }
+  console.log(favoriteShows);
+
+  //pintando favorites (luego sacar de aquí)
+  
+} 
+/* function handleShow(ev) {
+  const clickedShowId = ev.currentTarget.id;
+  // busco si la paleta clickada está en el array de favoritos
+  const favoritesFoundIndex = favorites.findIndex(favorite => favorite.id === clickedShowId);
+  // si la paleta no está en favoritos findIndex me ha devuelto -1
+  if (favoritesFoundIndex === -1) {
+    // busco la paleta clickada en el array de paletas
+    const showFound = shows.find(show => show.id === clickedShowId);
+    // para luego añadirlo al array de favoritos
+    favorites.push(showFound);
+  } else {
+    // si el findIndex me ha devuelto un número mayor o igual a 0 es que sí está en el array de favoritos
+    // quiero sacarlo de array de favoritos
+    // para utilizar splice necesito el índice del elemento que quiero borrar
+    // y quiero borrar un solo elemento
+    favorites.splice(favoritesFoundIndex, 1);
+  }
+  console.log(favorites);
+  // meter aqui función para pintar favoritos
+} */
